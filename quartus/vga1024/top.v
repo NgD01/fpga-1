34a8030ac3d1fa755a842f2bf896d09b91b0a82e
Jean-Claude Wippler <jc@wippler.nl>
2016-08-12 01:32:23 +0200

    fix whitespace

--- quartus/vga1024/top.v
+++ quartus/vga1024/top.v
@@ -1,29 +1,29 @@
 // Adapted from: www.TinyVGA.com
 
 module top (
-	input clk, // 50 MHz    
+	input clk, // 50 MHz
 	output [2:0] red,    
 	output [2:0] green,
 	output [1:0] blue,
 	output hsync, vsync     
 );
 
-reg [11:0] hcount;     // VGA horizontal counter
-reg [10:0] vcount;     // VGA vertical counter
-reg [7:0] data;		  // RGB data
+reg [11:0] hcount;  // VGA horizontal counter
+reg [10:0] vcount;  // VGA vertical counter
+reg [7:0] data;	    // RGB data
 
 wire hcount_ov, vcount_ov, video_active;
 
 // VGA mode parameters
 parameter
 	hsync_end   = 12'd119,
-   hdat_begin  = 12'd242,
-   hdat_end    = 12'd1266,
-   hpixel_end  = 12'd1345,
-   vsync_end   = 11'd5,
-   vdat_begin  = 11'd32,
-   vdat_end    = 11'd632,
-   vline_end   = 11'd665;
+    hdat_begin  = 12'd242,
+    hdat_end    = 12'd1266,
+    hpixel_end  = 12'd1345,
+    vsync_end   = 11'd5,
+    vdat_begin  = 11'd32,
+    vdat_end    = 11'd632,
+    vline_end   = 11'd665;
 
 always @(posedge clk)
 	if (hcount_ov)
