54ca04e4464faa4cd85d436f9579969cbbcbc85b
Jean-Claude Wippler <jc@wippler.nl>
2016-09-05 17:15:13 +0200

    simplify

--- quartus/tvout/top.v
+++ quartus/tvout/top.v
@@ -25,31 +25,26 @@ always @(posedge clk10) begin
         xpos <= xpos + 1;
 end
 
-localparam VISIBLE=2'b00, BLANKED=2'b01, PREPOST=2'b10, VSYNC=2'b11;
+localparam VISIBLE=2'b00, BLANKED=2'b01, VSYNC=2'b10;
 
 reg [1:0] mode;
 always @(*)
-    if (xpos < 490 && ypos < 268)
+    if (xpos < 492 && ypos < 268)
         mode = VISIBLE;
-//  else if (ypos < 270)
-//      mode = BLANKED;
     else if (ypos < 270)
-        mode = PREPOST;
+        mode = BLANKED;
     else if (ypos < 272)
         mode = VSYNC;
     else if (ypos == 272)
-        mode = xpos < 320 ? VSYNC : PREPOST;
-    else if (ypos < 275)
-        mode = PREPOST;
+        mode = xpos < 320 ? VSYNC : BLANKED;
     else
         mode = BLANKED;
 
 wire enable = mode == VISIBLE;
 wire vsync = mode == VSYNC;
-//wire hsync = (mode == visible || mode == blanked) && 528 <= xpos && xpos < 575;
-wire hsync = 528 <= xpos && xpos < 575;
+wire hsync = 529 <= xpos && xpos < 576;
 
-assign vout = enable && (xpos == 0 || xpos == 489 || ypos == 0 || ypos == 267);
+assign vout = enable && (xpos == 0 || xpos == 491 || ypos == 0 || ypos == 267);
 assign sync_ = enable || !(vsync || hsync);
 
 endmodule
