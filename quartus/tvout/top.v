30c47033c35bc74d759b03685ed91aa3e1911e0b
Jean-Claude Wippler <jc@wippler.nl>
2016-09-06 14:17:46 +0200

    saving fetch code, it "sort of" works...

--- quartus/tvout/top.v
+++ quartus/tvout/top.v
@@ -48,17 +48,47 @@ wire hSync = 532 <= xPos && xPos < 579;
 localparam XMIN=8, XMAX=495, YMIN=18, YMAX=283;
 wire vBars = xPos==XMIN || xPos==XMIN+10 || xPos==XMAX-10 || xPos==XMAX;
 wire hBars = yPos==YMIN || yPos==YMIN+10 || yPos==YMAX-10 || yPos==YMAX;
-wire vCut = vBars && YMIN <= yPos && yPos <= YMAX;
-wire hCut = hBars && XMIN <= xPos && xPos <= XMAX;
+wire vTest = vBars && YMIN <= yPos && yPos <= YMAX;
+wire hTest = hBars && XMIN <= xPos && xPos <= XMAX;
+
+reg [7:0] vData, vShift, vMem [512*288/8];  // 18 KB
+reg [14:0] vAddr;
 
 // delay by one cycle to perform the video ram fetch
 reg active_d, vout_d, sync_d;
-always @(posedge clk)
+always @(posedge clk) begin
+    if (fetchClk) begin
+        vAddr <= {yPos,xPos[8:3]};
+        if (vAddr == 15'h2001 || vAddr == 15'h2002 || vAddr == 15'h2003)
+            vMem[vAddr] <= 8'hAA;
+        else if (vAddr == 15'h2041 || vAddr == 15'h2042 || vAddr == 15'h2043)
+            vMem[vAddr] <= 8'hA0;
+        else if (vAddr == 15'h2081 || vAddr == 15'h2082 || vAddr == 15'h2083)
+            vMem[vAddr] <= 8'h80;
+        else if (vAddr == (YMIN*512+XMIN)/8 || vAddr == (YMIN*512+XMAX)/8 ||
+                 vAddr == (YMAX*512+XMIN)/8 || vAddr == (YMAX*512+XMAX)/8)
+            vMem[vAddr] <= 8'h81;
+        if (vAddr == 15'h1001 || vAddr == 15'h1041 || vAddr == 15'h1081)
+            vData <= 8'hAA;
+        else if (vAddr == 15'h103C || vAddr == 15'h107C || vAddr == 15'h10BC)
+            vData <= 8'h55;
+        else
+            vData <= vMem[vAddr];
+    end
+
     if (fetchClk) begin
         active_d = active;
-        vout_d <= vCut || hCut;
+        vout_d <= vTest || hTest;       // double border test
+//      vout_d <= xPos[3] ^ yPos[3];    // 8x8 checkerboard
+//      vout_d <= xPos[0] ^ yPos[0];    // 1x1 checkerboard
+//      vout_d <= xPos[0];              // fine vertical bars
+//      vout_d <= yPos[0];              // fine horizontal bars
         sync_d <= vSync || hSync;
-    end
+        vShift <= vData;
+    end else if (pixClk)
+        vShift <= {vShift[6:0],1'b0};
+//  vout_d = vShift[7];
+end
 
 assign vout = active_d && vout_d;
 assign sync_ = !sync_d;
