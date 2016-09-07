82d1b2c805d2e3b7ca4db907af3144999d6a2c42
Jean-Claude Wippler <jc@wippler.nl>
2016-09-08 00:43:41 +0200

    more cleanups

--- quartus/stiv-sdramtest/sdram_ctrl.v
+++ quartus/stiv-sdramtest/sdram_ctrl.v
@@ -30,100 +30,89 @@ module SdramCtrl (
     zs_dq
 );
 
-parameter     CHIP_ADDR_WIDTH         =  11;
-parameter     BANK_ADDR_WIDTH         =  2;
-parameter     ROW_WIDTH               =  12;
-parameter     COL_WIDTH               =  8;
-parameter     DATA_WIDTH              =  16;
-parameter     CAS_LATENCY             =  3'b011;
+parameter ADDR_WIDTH  = 11;
+parameter BANK_WIDTH  = 2;
+parameter ROW_WIDTH   = 11;
+parameter COL_WIDTH   = 9;
+parameter DATA_WIDTH  = 16;
+parameter CAS_LATENCY = 3'b011;
+
+localparam ROWCOL_WIDTH = ROW_WIDTH + COL_WIDTH;
 
 // Auto refresh cycle calculation: each row must be refreshed every 64ms, and
 // refresh one row each time. Since this chip has 8192 rows, the refresh
 // interval must be less than 64ms/8192 = 7.8125us. The 50Mhz clock should
 // count to 7.8125x1000/20 = 390, then do one refresh.
 
-parameter     AUTO_REFRESH_CYCLE      = 390;
-parameter     POWERON_WAIT_CYCLE      = 10000; // 200us, based on 50 MHz clock
+parameter AUTO_REFRESH_CYCLE = 390;
+parameter POWERON_WAIT_CYCLE = 10000; // 200us, based on 50 MHz clock
 
-input                     clk;
-input                     reset_l;
+input clk, reset_l, sdram_req;
 
-input                     sdram_req;
-output                    sdram_ack;
-input   [ROW_WIDTH+COL_WIDTH+BANK_ADDR_WIDTH-1:0] sdram_addr;
-input                     sdram_rh_wl;
-input   [DATA_WIDTH-1:0]  sdram_data_w;
-output  [DATA_WIDTH-1:0]  sdram_data_r;
-output                    sdram_data_r_en;
+input                                sdram_rh_wl;
+input  [ROWCOL_WIDTH+BANK_WIDTH-1:0] sdram_addr;
+input  [DATA_WIDTH-1:0]              sdram_data_w;
+output                               sdram_ack;
+output                               sdram_data_r_en;
+output [DATA_WIDTH-1:0]              sdram_data_r;
 
-output                        zs_ck;
-output                        zs_cke;
-output                        zs_cs_n;
-output                        zs_ras_n;
-output                        zs_cas_n;
-output                        zs_we_n;
-output  [BANK_ADDR_WIDTH-1:0] zs_ba;
-output  [CHIP_ADDR_WIDTH-1:0] zs_addr;
-output  [1:0]                 zs_dqm;
-inout   [DATA_WIDTH-1:0]      zs_dq;
+output zs_ck, zs_cke, zs_cs_n, zs_ras_n, zs_cas_n, zs_we_n;
+output [BANK_WIDTH-1:0] zs_ba;
+output [ADDR_WIDTH-1:0] zs_addr;
+output [1:0]            zs_dqm;
+inout  [DATA_WIDTH-1:0] zs_dq;
 
-wire                          zs_ck = clk;
-wire                          zs_cke = 1'b1;
+wire zs_ck = clk, zs_cke = 1'b1, zs_cs_n, zs_ras_n, zs_cas_n, zs_we_n;
 
-wire                          zs_cs_n;
-wire                          zs_ras_n;
-wire                          zs_cas_n;
-wire                          zs_we_n;
-reg     [3:0]                 sdram_cmd;
-reg     [BANK_ADDR_WIDTH-1:0] zs_ba;
-reg     [CHIP_ADDR_WIDTH-1:0] zs_addr;
-reg     [1:0]                 zs_dqm;
-reg                           zs_dq_o_en;
-reg     [DATA_WIDTH-1:0]      zs_dq_o;
-wire    [DATA_WIDTH-1:0]      zs_dq_i;
+reg  [3:0]            sdram_cmd;
+reg  [BANK_WIDTH-1:0] zs_ba;
+reg  [ADDR_WIDTH-1:0] zs_addr;
+reg  [1:0]            zs_dqm;
+reg                   zs_dq_o_en;
+reg  [DATA_WIDTH-1:0] zs_dq_o;
+wire [DATA_WIDTH-1:0] zs_dq_i;
 
-assign zs_dq = (zs_dq_o_en==1'b1)?zs_dq_o:{DATA_WIDTH{1'bz}};
+assign zs_dq = zs_dq_o_en == 1'b1 ? zs_dq_o : {DATA_WIDTH{1'bz}};
 assign zs_dq_i = zs_dq;
 assign {zs_cs_n, zs_ras_n, zs_cas_n, zs_we_n} = sdram_cmd;
 
-reg     [DATA_WIDTH-1:0]    sdram_data_r;
-reg                         sdram_data_r_en;
-reg                         sdram_ack;
+reg [DATA_WIDTH-1:0] sdram_data_r;
+reg                  sdram_data_r_en;
+reg                  sdram_ack;
 
-parameter   stat_poweron_wait   = 8'b00000001;
-parameter   stat_precharge      = 8'b00000010;
-parameter   stat_refresh        = 8'b00000100;
-parameter   stat_mrs            = 8'b00001000;
-parameter   stat_idle           = 8'b00010000;
-parameter   stat_active_row     = 8'b00100000;
-parameter   stat_read           = 8'b01000000;
-parameter   stat_write          = 8'b10000000;
+parameter stat_poweron_wait = 8'b00000001;
+parameter stat_precharge    = 8'b00000010;
+parameter stat_refresh      = 8'b00000100;
+parameter stat_mrs          = 8'b00001000;
+parameter stat_idle         = 8'b00010000;
+parameter stat_active_row   = 8'b00100000;
+parameter stat_read         = 8'b01000000;
+parameter stat_write        = 8'b10000000;
 
-reg     [7:0]   CUR_STATE;
-reg     [7:0]   NEXT_STATE;
+reg [7:0]  CUR_STATE;
+reg [7:0]  NEXT_STATE;
 
-reg             auto_refresh;
-reg   [15:0]    auto_refresh_cnt;
-reg             poweron_wait_ok;
-reg             init_ok;
-reg             precharge_done;
-reg             refresh_done;
-reg             mrs_done;
-reg             active_row_done;
-reg             read_done;
-reg             write_done;
+reg        auto_refresh;
+reg [15:0] auto_refresh_cnt;
+reg        poweron_wait_ok;
+reg        init_ok;
+reg        precharge_done;
+reg        refresh_done;
+reg        mrs_done;
+reg        active_row_done;
+reg        read_done;
+reg        write_done;
 
-reg   [15:0]    poweron_wait_cnt;
-reg   [3:0]     status_running_cnt;
+reg [15:0] poweron_wait_cnt;
+reg [3:0]  status_running_cnt;
 
-always @(negedge reset_l or posedge clk)
+always @(posedge clk or negedge reset_l)
     if (reset_l == 1'b0)
-        CUR_STATE <= stat_poweron_wait ;
+        CUR_STATE <= stat_poweron_wait;
     else
         CUR_STATE <= NEXT_STATE;
 
-always @(*) begin
-    NEXT_STATE <= stat_idle;
+always @(*)
     case (CUR_STATE)
         stat_poweron_wait:
             if (poweron_wait_ok == 1'b1)
@@ -137,7 +126,7 @@ always @(*) begin
                 else
                     NEXT_STATE <= stat_refresh;
             else
-                    NEXT_STATE <= stat_precharge;
+                NEXT_STATE <= stat_precharge;
         stat_refresh:
             if (refresh_done == 1'b1)
                 if (init_ok == 1'b1)
@@ -166,7 +155,7 @@ always @(*) begin
                 else
                     NEXT_STATE <= stat_write;
             else
-                    NEXT_STATE <= stat_active_row;
+                NEXT_STATE <= stat_active_row;
         stat_read:
             if (read_done == 1'b1)
                 NEXT_STATE <= stat_precharge;
@@ -178,24 +167,22 @@ always @(*) begin
             else
                 NEXT_STATE <= stat_write;
         default:
-            NEXT_STATE <= stat_idle ;
+            NEXT_STATE <= stat_idle;
     endcase
-end
 
 // sdram acknowledge control
-always @(negedge reset_l or posedge clk)
+always @(posedge clk or negedge reset_l)
     if (reset_l == 1'b0)
         sdram_ack <= 1'b0;
-    else begin
+    else if (CUR_STATE == stat_active_row)
+        sdram_ack <= 1'b1;
+    else if (sdram_req == 1'b1)
         sdram_ack <= 1'b0;
-        if (CUR_STATE == stat_active_row)
-            sdram_ack <= 1'b1;
-        else if (sdram_req == 1'b1)
-            sdram_ack <= 1'b0;
-    end
+    else
+        sdram_ack <= 1'b0;
 
 // stat_poweron_wait
-always @(negedge reset_l or posedge clk)
+always @(posedge clk or negedge reset_l)
     if (reset_l == 1'b0) begin
         poweron_wait_cnt <= 16'b0;
         poweron_wait_ok <= 1'b0;
@@ -205,19 +192,19 @@ always @(negedge reset_l or posedge clk)
             if (poweron_wait_cnt >= POWERON_WAIT_CYCLE)
                 poweron_wait_ok <= 1'b1;
             else
-                poweron_wait_cnt <= poweron_wait_cnt + 1;
+                poweron_wait_cnt <= poweron_wait_cnt + 16'b1;
         else
             poweron_wait_cnt <= 16'b0;
     end
 
 // auto refresh control
-always @(negedge reset_l or posedge clk)
+always @(posedge clk or negedge reset_l)
     if (reset_l == 1'b0) begin
         auto_refresh_cnt <= 16'b0;
         auto_refresh <= 1'b0;
     end else begin
         if (auto_refresh == 1'b0)
-            auto_refresh_cnt <= auto_refresh_cnt + 1;
+            auto_refresh_cnt <= auto_refresh_cnt + 16'b1;
         else
             auto_refresh_cnt <= 16'b0;
         if (auto_refresh_cnt >= AUTO_REFRESH_CYCLE)
@@ -227,7 +214,7 @@ always @(negedge reset_l or posedge clk)
     end
 
 // status running control
-always @(negedge reset_l or posedge clk)
+always @(posedge clk or negedge reset_l)
     if (reset_l == 1'b0)
         status_running_cnt <= 4'b0;
     else if (precharge_done || refresh_done || mrs_done ||
@@ -241,11 +228,11 @@ always @(negedge reset_l or posedge clk)
         status_running_cnt <= 4'b0;
 
 // other status control
-always @(negedge reset_l or posedge clk) begin
+always @(posedge clk or negedge reset_l) begin
     if (reset_l == 1'b0) begin
         sdram_cmd <= {4{1'b1}};
-        zs_ba <= {BANK_ADDR_WIDTH{1'b0}};
-        zs_addr <= {CHIP_ADDR_WIDTH{1'b0}};
+        zs_ba <= {BANK_WIDTH{1'b0}};
+        zs_addr <= {ADDR_WIDTH{1'b0}};
         zs_dqm <= 2'b0;
         zs_dq_o_en <= 1'b0;
         zs_dq_o <= {DATA_WIDTH{1'b0}};
@@ -267,8 +254,7 @@ always @(negedge reset_l or posedge clk) begin
         active_row_done <= 1'b0;
         read_done <= 1'b0;
         write_done <= 1'b0;
-        zs_ba <=
-          sdram_addr[ROW_WIDTH+COL_WIDTH+BANK_ADDR_WIDTH-1:ROW_WIDTH+COL_WIDTH];
+        zs_ba <= sdram_addr[ROWCOL_WIDTH+BANK_WIDTH-1:ROWCOL_WIDTH];
         zs_dq_o_en <= 1'b0;
         sdram_data_r_en <= 1'b0;
         case (CUR_STATE)
@@ -277,54 +263,51 @@ always @(negedge reset_l or posedge clk) begin
                 zs_addr[10] <= 1'b1;
                 precharge_done <= 1'b1;
             end
-            stat_refresh: begin
+            stat_refresh:
                 if (status_running_cnt == 4'b0)
                     sdram_cmd <= 4'b0001;
-                else
+                else begin
                     sdram_cmd <= 4'b0111; // noop operating mode
-                if (status_running_cnt >= 4'd8)
-                    refresh_done <= 1'b1;
-            end
-            stat_mrs: begin
+                    if (status_running_cnt >= 4'd8)
+                        refresh_done <= 1'b1;
+                end
+            stat_mrs:
                 if (status_running_cnt == 4'b0) begin
                     sdram_cmd <= 4'b0000;
-                    zs_addr <= {{3{1'b0}},1'b0,2'b00,CAS_LATENCY,4'h0};
-                end else
+                    zs_addr <= {1'b0,1'b0,2'b00,CAS_LATENCY,4'h0};
+                end else begin
                     sdram_cmd <= 4'b0111; // noop operating mode
-                if (status_running_cnt >= 4'd3) begin
-                    mrs_done <= 1'b1;
-                    init_ok <= 1'b1;
+                    if (status_running_cnt >= 4'd3) begin
+                        mrs_done <= 1'b1;
+                        init_ok <= 1'b1;
+                    end
                 end
-            end
             stat_active_row: begin
                 sdram_cmd <= 4'b0011;
-                zs_addr <= sdram_addr[ROW_WIDTH+COL_WIDTH-1:COL_WIDTH];
+                zs_addr <= sdram_addr[ROWCOL_WIDTH-1:COL_WIDTH];
                 active_row_done <= 1'b1;
             end
-            stat_read: begin
+            stat_read:
                 if (status_running_cnt == 4'd0) begin
                     sdram_cmd <= 4'b0101;
                     zs_addr <= sdram_addr[COL_WIDTH-1:0];
-                end
-                if (status_running_cnt == 4'd3) begin
+                end else if (status_running_cnt == 4'd3) begin
                     read_done <= 1'b1;
                     sdram_data_r_en <= 1'b1;
                     sdram_data_r <= zs_dq_i;
                 end
-            end
             stat_write: begin
                 zs_dq_o_en <= 1'b1;
                 if (status_running_cnt == 4'd0) begin
                     sdram_cmd <= 4'b0100;
                     zs_addr <= sdram_addr[COL_WIDTH-1:0];
                     zs_dq_o <= sdram_data_w;
-                end
-                if (status_running_cnt == 4'd1)
+                end else if (status_running_cnt == 4'd1)
                     write_done <= 1'b1;
             end
             stat_idle: begin
                 sdram_cmd <= 4'b1111; // command disable
-                zs_addr <= {CHIP_ADDR_WIDTH{1'b0}};
+                zs_addr <= {ADDR_WIDTH{1'b0}};
             end
             default: ;
         endcase
--- quartus/stiv-sdramtest/sdram_test.v
+++ quartus/stiv-sdramtest/sdram_test.v
@@ -10,23 +10,23 @@ module top (
     zs_ba,
     zs_addr,
     zs_dqm,
-    zs_dq ,
+    zs_dq,
     led
 );
 
-input                           clk          ;
-input                           reset_l      ;
-output                          zs_ck        ;
-output                          zs_cke       ;
-output                          zs_cs_n      ;
-output                          zs_ras_n     ;
-output                          zs_cas_n     ;
-output                          zs_we_n      ;
-output       [1:0]              zs_ba        ;
-output       [11:0]             zs_addr      ;
-output       [1:0]              zs_dqm       ;
-inout        [15:0]             zs_dq        ;
-output  reg  [7:0]              led          ;
+input             clk;
+input             reset_l;
+output            zs_ck;
+output            zs_cke;
+output            zs_cs_n;
+output            zs_ras_n;
+output            zs_cas_n;
+output            zs_we_n;
+output     [1:0]  zs_ba;
+output     [11:0] zs_addr;
+output     [1:0]  zs_dqm;
+inout      [15:0] zs_dq;
+output reg [7:0]  led;
 
 // three state
 parameter   IDLE   =  3'b001;
@@ -37,23 +37,21 @@ parameter  div_400us = 15'd25000; // delay number,for initialization of sdram
 parameter  sdram_addr_num = 24'b0; // row col bank: 13+9+2
 parameter  sdram_data_w_num = 16'b1111000001010101; // f055
 
-reg                              sdram_req         ;
-reg        [23:0]                sdram_addr        ;
-reg                              sdram_rh_wl       ;
-reg        [15:0]                sdram_data_w      ;
-reg        [2:0]                 current_state     ;
-reg        [2:0]                 current_state_dly1;
-reg        [2:0]                 next_state        ;
-reg        [14:0]                init_wait_cnt     ;
-reg        [7:0]                 wr_cnt            ;
-reg                              sdram_data_r_en_1 ;
-reg                              sdram_ack_1;
-reg        [15:0]                sdram_data_r_lock ;
-wire       [15:0]                sdram_data_r      ;
-wire                             sdram_data_r_en   ;
-wire                             sdram_ack         ;
-wire                             rd_en             ;
-wire                             wr_en             ;
+reg          sdram_req;
+reg  [23:0]  sdram_addr;
+reg          sdram_rh_wl;
+reg  [15:0]  sdram_data_w;
+reg  [2:0]   current_state;
+reg  [2:0]   current_state_dly1;
+reg  [2:0]   next_state;
+reg  [14:0]  init_wait_cnt;
+reg  [7:0]   wr_cnt;
+reg  [15:0]  sdram_data_r_lock;
+wire [15:0]  sdram_data_r;
+wire         sdram_data_r_en;
+wire         sdram_ack;
+wire         rd_en;
+wire         wr_en;
 
 always @(posedge clk or negedge reset_l)
     if (!reset_l)
@@ -65,7 +63,7 @@ always @(posedge clk or negedge reset_l)
     if (!reset_l)
         init_wait_cnt<=15'b0;
     else if (init_wait_cnt<= (div_400us-1))
-        init_wait_cnt<=init_wait_cnt+1;
+        init_wait_cnt<=init_wait_cnt+15'b1;
 
 always @(posedge clk or negedge reset_l)
     if (!reset_l)
@@ -89,28 +87,24 @@ always @(*)
             next_state=READ;
    endcase
 
-// delay button's signal ,make this stable
+// delay button's signal, make this stable
 always @(posedge clk or negedge reset_l)
     if (!reset_l)
         current_state_dly1 <=3'b0;
     else
         current_state_dly1<=current_state;
 
-assign wr_en =  (current_state == WRITE) & (current_state_dly1 != WRITE) ;
+assign wr_en =  (current_state == WRITE) & (current_state_dly1 != WRITE);
 
-assign rd_en =  (current_state == READ) & (current_state_dly1 != READ) ;
+assign rd_en =  (current_state == READ) & (current_state_dly1 != READ);
 
 always @(posedge clk or negedge reset_l)
     if (!reset_l) begin
         sdram_rh_wl<=1'b1;
         sdram_req<=1'b0;
-        sdram_data_r_en_1<=1'b0;
-        sdram_ack_1<=1'b0;
         sdram_addr<=0;
         sdram_data_w<=0;
     end else if (current_state == WRITE) begin
-        sdram_data_r_en_1<=sdram_data_r_en;
-        sdram_ack_1<=sdram_ack;
         sdram_addr   <=sdram_addr_num;
         sdram_data_w <=sdram_data_w_num;
         sdram_req    <= wr_en;
@@ -118,8 +112,6 @@ always @(posedge clk or negedge reset_l)
     end else if (current_state == READ) begin
         sdram_rh_wl  <=1'b1;
         sdram_req    <= rd_en;
-        sdram_data_r_en_1<=sdram_data_r_en;
-        sdram_ack_1<=sdram_ack;
     end
 
 always @(posedge clk or negedge reset_l)
