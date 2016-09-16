c0b3900da4bd245205fd7155d81bfad82e4eeb2b
Jean-Claude Wippler <jc@wippler.nl>
2016-09-16 12:44:44 +0200

    add pulse generator for use with a resistor divider

---
+++ quartus/stiv-fsmc/tests/e-pulse.fs
@@ -0,0 +1,17 @@
+\ generate a square wave of adjustable frequency
+
+reset
+
+\ pins used:
+\   C2 A0 A2 A4 A6
+\   C1 C3 A1 A3 A5
+\
+\ C3 = 1kΩ to A0, 1kΩ to A1, 1kΩ to A2, where: A0 = PWM, A1 = "0", A2 = "1"
+
+: init-timer
+  7200 PA0 +pwm  9000 PA0 pwm  \ set up timer 2 with a fairly low frequency
+  OMODE-PP PA1 io-mode!  0 PA1 io!
+  OMODE-PP PA2 io-mode!  1 PA2 io!
+;
+
+init-timer
